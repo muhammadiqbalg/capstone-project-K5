eb7131145d4d62efce02882f4cf25886fbc843ec
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2023-10-30 11:48:17 +0700

    add: file project & feat:akun

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'rendy.firmans28@gmail.com',
+            'password' => bcrypt('12345678'),
+            'usertype' => 'admin'
+        ]);
+    }
+}
